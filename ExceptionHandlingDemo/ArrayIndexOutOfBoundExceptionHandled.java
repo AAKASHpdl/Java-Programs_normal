d40dc40ad6e92945dc603874a711350f11d47d04
bijay135 <bijay.shah975@gmail.com>
2017-07-23 17:40:55 +0545

    Update ArrayIndexOutOfBoundExceptionHandled.java

--- ExceptionHandlingDemo/ArrayIndexOutOfBoundExceptionHandled.java
+++ ExceptionHandlingDemo/ArrayIndexOutOfBoundExceptionHandled.java
@@ -11,8 +11,9 @@ public class ArrayIndexOutOfBoundExceptionHandled {
                 System.out.println("Enter a integer: ");
                 n=S.nextInt();
                 System.out.println("They array index is 0 to 2");
-                System.out.println("Enter a Index you can also enter OutOfBound Index");
-                System.out.println("The program will handle the excepetion");
+                System.out.println("Enter a Index to save the integer to");
+                System.out.println("You can also enter OutOfBound Index");
+                System.out.println("The program will handle the excepetion: ");
                 i=S.nextInt();
                 number[i]=n;
                 System.out.println();
@@ -24,7 +25,7 @@ public class ArrayIndexOutOfBoundExceptionHandled {
                 System.err.println("Exception: "+e.toString());
                 System.out.println("Array Index is out of range");
                 System.out.println("Please enter the array index within range to save integer to");
-                System.out.println("15");
+                System.out.println();
             }
          }while(continueLoop);
     }
