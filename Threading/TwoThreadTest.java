aef994e1a7605c77ec09f00b15347fdf19b783a2
bijay135 <bijay.shah975@gmail.com>
2017-08-10 21:23:10 +0545

    Delete TwoThreadTest.java

--- Threading/TwoThreadTest.java
+++
@@ -1,7 +0,0 @@
-package Threading;
-public class TwoThreadTest {
-    public static void main (String[] args) {
-        new ThreadingDemo("Jamaica").start();
-        new ThreadingDemo("Fiji").start();
-    }
-}
