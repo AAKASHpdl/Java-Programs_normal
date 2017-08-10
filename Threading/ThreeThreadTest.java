480bfeb57c736ea869415aa93f19b52dab7c3eb8
bijay135 <bijay.shah975@gmail.com>
2017-08-10 21:23:02 +0545

    Delete ThreeThreadTest.java

--- Threading/ThreeThreadTest.java
+++
@@ -1,8 +0,0 @@
-package Threading;
-public class ThreeThreadTest {
-     public static void main (String[] args) {
-        new ThreadingDemo("Jamaica").start();
-        new ThreadingDemo("Fiji").start();
-        new ThreadingDemo("Bora Bora").start();
-    }
-}
